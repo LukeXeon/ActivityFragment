c4e3fd5931c1f7f354da5d0d3f6d87ee894b51bb
luotinghui <luotinghui@bilibili.com>
2021-09-19 00:57:02 +0800

    m

--- activityfragment/src/main/java/open/source/uikit/activityfragment/StubActivity.kt
+++ activityfragment/src/main/java/open/source/uikit/activityfragment/StubActivity.kt
@@ -1,7 +1,14 @@
 package open.source.uikit.activityfragment
 
 import android.app.Activity
+import android.os.Bundle
 import androidx.annotation.RestrictTo
 
+@Deprecated("Used only as a stub.", level = DeprecationLevel.HIDDEN)
 @RestrictTo(RestrictTo.Scope.LIBRARY)
-internal class StubActivity : Activity()
+internal class StubActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish()
+    }
+}
